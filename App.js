3eef5408f4b797c4c17eaea628200b224134028d
rutujanigade <rnigade@csumb.edu>
2024-11-18 16:29:53 -0800

    searchpage for freshlist

--- App.js
+++ App.js
@@ -1,26 +1,32 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 import { StatusBar } from 'expo-status-bar';
 import { NavigationContainer } from '@react-navigation/native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
-import Homepage from './assets/pages/Homepage'; 
+import Homepage from './assets/pages/Homepage';
+import Searchpage from './assets/pages/Searchpage';
 
 const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <LinearGradient
-        colors={['#2fe97f', '#56ab2f']} 
-        style={styles.container}
+      <Drawer.Navigator 
+        initialRouteName="Home" 
+        screenOptions={{
+          swipeEnabled: true, 
+          gestureEnabled: true, 
+          headerShown: false, 
+        }}
       >
-        <Drawer.Navigator initialRouteName="Home">
-          <Drawer.Screen name="Home" component={Homepage} />
-          {/* You can add more screens here as needed */}
-        </Drawer.Navigator>
-        <StatusBar style="auto" />
-      </LinearGradient>
+        <Drawer.Screen name="Home" component={Homepage} />
+        <Drawer.Screen name="Search" component={Searchpage} />
+        {/* You can add more screens here as needed */}
+      </Drawer.Navigator>
+
+      {/* LinearGradient */}
+      <StatusBar style="auto" />
     </NavigationContainer>
   );
 }
@@ -29,10 +35,4 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
-  text: {
-    color: 'white',
-    fontSize: 24,
-    fontWeight: 'bold',
-  },
 });
-
